9b9a2fe7cb3326ab821df4e201635b61cf43ba67
wangzixiang <wangzixiang@onething.net>
2023-09-24 15:50:40 +0800

    增加更多显示信息

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -8,16 +8,12 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
   spec = {
-    -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.go" },
-    -- import/override with your plugins
     { import = "plugins" },
   },
   defaults = {
