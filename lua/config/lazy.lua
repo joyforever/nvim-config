c0879eb37e6f1f342258026e44a27498a92ec0d1
wangzixiang <wangzixiang@onething.net>
2023-10-06 11:26:07 +0800

    使用独立副本的go配置

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,7 +22,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.rust" },
-    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "plugins" },
   },
