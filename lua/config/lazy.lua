d3496f89d74428036f4124431bf03373c8f31f4e
wangzixiang <wangzixiang@onething.net>
2024-04-08 10:44:42 +0800

    update plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,7 +19,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.rust" },
